9301185fc9ea431f0e9e4b0505f515dd658ee9e2
admatthi <alek.matthiessen@gmail.com>
2022-10-12 07:06:39 -0400

    latest code

--- SportsQuant/AppDelegate.swift
+++ SportsQuant/AppDelegate.swift
@@ -6,6 +6,29 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import Firebase
+import AudioToolbox
+import AVFoundation
+import Firebase
+import Purchases
+import FBSDKCoreKit
+import MBProgressHUD
+import AVKit
+import AVFoundation
+import FirebaseDatabase
+import FirebaseFirestore
+import AppTrackingTransparency
+import AdSupport
+import Amplitude_iOS
+import SVProgressHUD
+import AppsFlyerLib
+import SwiftyJSON
+
+var uid = String()
+
+var db : Firestore!
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,10 +37,71 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        db = Firestore.firestore()
+        
+        Purchases.debugLogsEnabled = true
+        
+        uid = UIDevice.current.identifierForVendor!.uuidString
+
+        Purchases.debugLogsEnabled = true
+
+        Purchases.configure(withAPIKey: "appl_AozYIhYOTjSTvONMDSUXOWFflCh", appUserID: uid)
+        Purchases.shared.delegate = self
+        
+        ApplicationDelegate.shared.application(
+                           application,
+                           didFinishLaunchingWithOptions: launchOptions
+                       )
+        
+        Purchases.shared.purchaserInfo { (purchaserInfo, error) in
+            if let purchaserInfo = purchaserInfo{
+                
+                
+                if !purchaserInfo.entitlements.active.isEmpty {
+                    
+                    if purchaserInfo.entitlements.active.first?.value.periodType == .trial {
+                        // here trail implementation
+                        
+                        
+                    }else if purchaserInfo.entitlements.active.first?.value.periodType == .normal {
+                        // here normal implementation
+                        //here
+                        AppEvents.logEvent(.purchased, parameters: ["":""])
+
+                        print("purchased")
+                    }else if purchaserInfo.entitlements.active.first?.value.periodType == .intro {
+                        // here intro implementation
+
+                    }else if purchaserInfo.entitlements.active.first?.value.periodType == .none {
+                        // here none implementation
+
+                    }
+                    //user has access to some entitlement
+                    didpurchase = true
+                    UserDefaults.standard.set(true, forKey: "didPurchase")
+                }
+            }
+        }
+        
+        
         return true
     }
 
 
 
+}
+
+extension AppDelegate: PurchasesDelegate {
+    func purchases(_ purchases: Purchases, didReceiveUpdated purchaserInfo: Purchases.PurchaserInfo) {
+        let purchaserInfo = purchaserInfo
+//        if !purchaserInfo.entitlements.active.isEmpty {
+//            //user has access to some entitlement
+//            didpurchase = true
+//            UserDefaults.standard.set(true, forKey: "didPurchase")
+//        }
+
+    }
 }
 
