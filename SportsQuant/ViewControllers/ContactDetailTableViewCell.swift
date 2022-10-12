1e1fa1a649e18f179cd75d93be7158e76a2071cb
talal ahmad <talalahmad321@gmail.com>
2022-10-12 22:38:34 +0500

    latest push

---
+++ SportsQuant/ViewControllers/ContactDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContactDetailTableViewCell.swift
+//  SportsQuant
+//
+//  Created by talal ahmad on 12/10/2022.
+//
+
+import UIKit
+
+class ContactDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- SportsQuant/ViewControllers/ContactViewController.swift
+++ SportsQuant/ViewControllers/ContactViewController.swift
@@ -8,14 +8,22 @@
 import UIKit
 
 class ContactViewController: UIViewController {
+    @IBOutlet weak var nextButton: UIButton!
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
+        nextButton.layer.cornerRadius = nextButton.frame.height/2
+        nextButton.layer.masksToBounds = true
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func nextButtonAction(_ sender: Any) {
+        pagingViewController?.select(index: 1,animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -26,4 +34,16 @@ class ContactViewController: UIViewController {
     }
     */
 
+}
+extension ContactViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactDetailTableViewCell", for: indexPath) as! ContactDetailTableViewCell
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 1000
+    }
 }
